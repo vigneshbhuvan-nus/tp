f2cfd5faadd44045ac833faaf8cfbc4e59935268
vigneshbhuvan-nus <e0323321@u.nus.edu>
2020-10-14 01:43:55 +0800

    Edited JsonAdaptedDeck

--- src/main/java/seedu/address/storage/JsonAdaptedDeck.java
+++ src/main/java/seedu/address/storage/JsonAdaptedDeck.java
@@ -3,12 +3,13 @@ package seedu.address.storage;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javafx.collections.ObservableList;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.deck.Deck;
 import seedu.address.model.deck.DeckName;
-import seedu.address.model.deck.entry.Entry;
 import seedu.address.model.deck.entry.UniqueEntryList;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -18,24 +19,29 @@ class JsonAdaptedDeck {
 
     public static final String MISSING_FIELD_MESSAGE_FORMAT = "Entry's %s field is missing!";
     private final String deckName;
-    private final ObservableList<Entry> internalList;
+    private final List<JsonAdaptedEntry> internalList = new ArrayList<>();
 
     /**
      * Constructs a {@code JsonAdaptedDeck} with the given entry details.
      */
     @JsonCreator
     public JsonAdaptedDeck(@JsonProperty("deckname") String deckName,
-                           @JsonProperty("internallist") ObservableList<Entry> internalList) {
+                           @JsonProperty("internallist") List<JsonAdaptedEntry> internalList) {
         this.deckName = deckName;
-        this.internalList = internalList;
+
+        if(internalList != null) {
+            this.internalList.addAll(internalList);
+        }
     }
 
     /**
      * Converts a given {@code Deck} into this class for Jackson use.
      */
-    public JsonAdaptedDeck(Deck deck) {
-        this.deckName = deck.getDeckName().deckName;
-        this.internalList = deck.getEntries().asUnmodifiableObservableList();
+    public JsonAdaptedDeck(Deck source) {
+        deckName = source.getDeckName().deckName;
+        internalList.addAll(source.getEntryList().stream()
+                .map(JsonAdaptedEntry::new)
+                .collect(Collectors.toList()));
     }
 
     /**
@@ -44,6 +50,11 @@ class JsonAdaptedDeck {
      * @throws IllegalValueException if there were any data constraints violated in the adapted entry.
      */
     public Deck toModelType() throws IllegalValueException {
+        final UniqueEntryList modelInternalList = new UniqueEntryList();
+
+        for (JsonAdaptedEntry entry : internalList) {
+            modelInternalList.add(entry.toModelType());
+        }
 
         if (deckName == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
@@ -53,15 +64,9 @@ class JsonAdaptedDeck {
             throw new IllegalValueException(DeckName.MESSAGE_CONSTRAINTS);
         }
         final DeckName modelDeckName = new DeckName(deckName);
-        if (internalList == null) {
-            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
-                    UniqueEntryList.class.getSimpleName()));
-        }
-        final UniqueEntryList modelEntries = new UniqueEntryList();
-        modelEntries.setEntries(internalList);
+
         final Deck deck = new Deck(modelDeckName);
-        deck.setEntries(modelEntries);
+        deck.setEntries(modelInternalList);
         return deck;
     }
-
 }
