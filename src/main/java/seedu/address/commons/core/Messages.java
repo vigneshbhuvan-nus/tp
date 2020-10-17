c2a6f2d69cf3f448f03a86348cb2558e3f5e64a6
geoboom <leegeorgie52@gmail.com>
2020-10-17 22:35:21 +0800

    Update dg toc

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -10,6 +10,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_ENTRY_DISPLAYED_INDEX = "The entry index provided is invalid";
     public static final String MESSAGE_INVALID_DECK_DISPLAYED_INDEX = "The deck index provided is invalid";
     public static final String MESSAGE_NO_DECK_SELECTED = "Please select a deck first. Select <index>";
+
     public static final String MESSAGE_ENTRIES_LISTED_OVERVIEW = "%1$d entries listed!";
 
 }
