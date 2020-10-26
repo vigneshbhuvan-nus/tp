21309ea3014f40fc65816d5b581271ac28184a12
Melanie <melanienps@gmail.com>
2020-10-26 18:11:40 +0800

    Add icon for start panel

--- src/main/java/seedu/address/ui/panels/StartPanel.java
+++ src/main/java/seedu/address/ui/panels/StartPanel.java
@@ -2,12 +2,17 @@ package seedu.address.ui.panels;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
 import seedu.address.ui.UiPart;
 
 public class StartPanel extends UiPart<Region> {
 
     private static final String FXML = "StartPanel.fxml";
+    
+    @FXML
+    private ImageView greenTeaIcon;
 
     @FXML
     private Label headerMessage;
@@ -20,6 +25,8 @@ public class StartPanel extends UiPart<Region> {
      */
     public StartPanel() {
         super(FXML);
+        Image greenTea = new Image(this.getClass().getResourceAsStream("/images/green_tea_icon.png"));
+        greenTeaIcon.setImage(greenTea);
         headerMessage.setText("Welcome to Green Tea!\n");
         commandMessage.setText("Here are some commands you can try:\n"
                 + "-----------------------------------------\n"
