41bb5c34be71fd7554aa6e27eb7b5b6bfea6e34d
Melanie <melanienps@gmail.com>
2020-11-04 14:55:34 +0800

    Fix commented out SampleDataUtil to show sample address book when json file is missing.

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -2,6 +2,8 @@ package seedu.address.model.util;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.deck.Deck;
+import seedu.address.model.deck.DeckName;
 import seedu.address.model.deck.entry.Entry;
 import seedu.address.model.deck.entry.Translation;
 import seedu.address.model.deck.entry.Word;
@@ -10,23 +12,42 @@ import seedu.address.model.deck.entry.Word;
  * Contains utility methods for populating {@code AddressBook} with sample data.
  */
 public class SampleDataUtil {
-    public static Entry[] getSampleEntries() {
+    public static Entry[] getSampleEntriesJapanese() {
         return new Entry[] {
-            new Entry(new Word("Alex Yeoh"), new Translation("alexyeoh@example.com")),
-            new Entry(new Word("Bernice Yu"), new Translation("berniceyu@example.com")),
-            new Entry(new Word("Charlotte Oliveiro"), new Translation("charlotte@example.com")),
-            new Entry(new Word("David Li"), new Translation("lidavid@example.com")),
-            new Entry(new Word("Irfan Ibrahim"), new Translation("irfan@example.com")),
-            new Entry(new Word("Roy Balakrishnan"), new Translation("royb@example.com"))
+            new Entry(new Word("Apple"), new Translation("りんご")),
+            new Entry(new Word("Bridge"), new Translation("橋")),
+            new Entry(new Word("Cat"), new Translation("猫")),
         };
+    }
+
+    public static Entry[] getSampleEntriesSpanish() {
+        return new Entry[] {
+           new Entry(new Word("Summer"), new Translation("verano")),
+           new Entry(new Word("Winter"), new Translation("invierno")),
+           new Entry(new Word("Spring"), new Translation("primavera")),
+        };
+    }
+
+    public static Deck getSampleJapaneseDeck() {
+        Deck sampleJapaneseDeck = new Deck(new DeckName("Japanese"));
+        for (Entry sampleJapaneseEntry : getSampleEntriesJapanese()) {
+            sampleJapaneseDeck.addEntry(sampleJapaneseEntry);
+        }
+        return sampleJapaneseDeck;
+    }
+
+    public static Deck getSampleSpanishDeck() {
+        Deck sampleSpanishDeck = new Deck(new DeckName("Spanish"));
+        for (Entry sampleSpanishEntry: getSampleEntriesSpanish()) {
+            sampleSpanishDeck.addEntry(sampleSpanishEntry);
+        }
+        return sampleSpanishDeck;
     }
 
     public static ReadOnlyAddressBook getSampleAddressBook() {
         AddressBook sampleAb = new AddressBook();
-        for (Entry sampleEntry : getSampleEntries()) {
-            /*sampleAb.addEntry(sampleEntry);
-        */}
-        //sampleAb.addEntry(sampleEntry);
+        sampleAb.addDeck(getSampleJapaneseDeck());
+        sampleAb.addDeck(getSampleSpanishDeck());
         return sampleAb;
     }
 }
