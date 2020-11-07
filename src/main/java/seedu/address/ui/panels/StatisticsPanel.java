5e1158d9a1b570d6245fce830619e6c636db3ea9
Gabriel Sim <gabrielsimbingyang@gmail.com>
2020-11-07 11:04:37 +0800

    Edit CI Checks

--- src/main/java/seedu/address/ui/panels/StatisticsPanel.java
+++ src/main/java/seedu/address/ui/panels/StatisticsPanel.java
@@ -3,13 +3,9 @@ package seedu.address.ui.panels;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
